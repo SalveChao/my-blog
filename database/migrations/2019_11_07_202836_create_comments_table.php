a80606aec4130ea914e112279ab02d00432dfcbf
salve.chao <salve-chao@live.jp>
2019-11-07 11:36:18 +0000

    add migration files for project

---
+++ database/migrations/2019_11_07_202836_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->text('comment');
+            // $table->boolean('approved');
+            $table->integer('post_id')->nullable()->unsigned()->index();
+            $table->timestamps();
+            $table->softDeletes();
+
+             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
