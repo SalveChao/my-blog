cf459e50d85ea58cf0bc72453032564cf08b2e13
salve.chao <salve-chao@live.jp>
2019-11-30 12:04:43 +0000

    modified Archive.php

--- app/Models/Archive.php
+++ app/Models/Archive.php
@@ -14,8 +14,8 @@ class Archive extends Model
        $archives_list = DB::table('posts')
             ->whereNull('deleted_at')
             ->selectRaw('
-                year("created_at" without time zone) as year,
-                month("created_at" without time zone) as month, 
+                year(extract(created_at ) as year,
+                month(extract(created_at)) as month, 
                 COUNT(*) as post_count
             ')
             ->groupBy('year')
